c2a76abd60a20ee4950021559895eb6aafcd025a
GustavoDamiani1407 <gustavodamiani1407@gmail.com>
2025-07-12 15:02:06 -0300

    update

--- bio/sequencia.py
+++ bio/sequencia.py
@@ -51,3 +51,13 @@ class Sequencia:
     def transcrever(self):
         return self.sequencia.replace('T', 'U')
     
+    def traduzir(self):
+        aminoacidos = []
+        for i in range(0, len(self.sequencia) - 2, 3):
+            codon = self.sequencia[i:i+3]
+            if codon in DNA_STOP_CODONS:
+                aminoacidos.append('STOP')
+            aminoacido = DNA_PARA_AMINOACIDO.get(codon, None)
+            if aminoacido:
+                aminoacidos.append(aminoacido)
+    
